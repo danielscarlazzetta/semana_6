ca2a0994698eadca066c4025bcc1b16574e7e268
danielscarlazzetta <104595375+danielscarlazzetta@users.noreply.github.com>
2026-04-20 22:55:37 -0400

    create a index, style and JS
    Esta agradable noche aprendimos el console log, junto a trabajr con las operaciones logicas, como lo son el  Y, Ó, matematicas y distintos

---
+++ src/javascript/01-matematicas.js
@@ -0,0 +1,80 @@
+
+//Definimos las vaiables
+let x = 2;
+let y = 4;
+
+//Mostrar el varlor de cada variable
+console.log('La variable de "X" tiene un valor de: ', x);
+console.log("La variable de 'Y' tiene un valor de: ", y);
+
+console.log("----------------------- ");
+
+//Operaciones Matematicas
+console.log( "Suma: ", x + y );
+console.log( "Resta: ", x - y );
+console.log( "Divicion: ", x / y );
+console.log( "Multiplicación: ", x * y );
+
+//ransformamos el int en un String
+console.log( "Parse: " + (x * y ));
+
+//Definimos Booleans
+let t = true;
+let f = false;
+
+//Comparativa de valores Booleanos
+console.log("Si comparamos t con t nos da: ", t == t);
+console.log("Si comparamos t con f nos da: ", t == f);
+console.log("Si comparamos f con f nos da: ", f == f);
+
+console.log("---------------- ");
+
+//Comparativa una u otra
+console.log("Si es t ó t nos da: ", t || t);
+console.log("Si es t ó f nos da: ", t || f);
+console.log("Si es f ó f nos da: ", f || f);
+
+console.log("---------------- ");
+
+//Cumplir dos condiciones
+console.log("Si elegimos entre t y t tenemos: ", t && t);
+console.log("Si elegimos entre t y f tenemos: ", t && f);
+console.log("Si elegimos entre f y f tenemos: ", f && f);
+
+console.log("---------------- ");
+
+//Distinto
+console.log( t != t );
+console.log( t != f );
+console.log( f != f );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
